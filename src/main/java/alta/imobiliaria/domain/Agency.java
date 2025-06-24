f92553ab6234bdd1c6b2ea88129e3730f7f013fe
Deyvid Lucas Amorim <deyvidlucas_026@outlook.com>
2025-06-24 19:12:01 -0300

    fix: avoid duplicate filter definition

--- src/main/java/alta/imobiliaria/domain/Agency.java
+++ src/main/java/alta/imobiliaria/domain/Agency.java
@@ -2,10 +2,13 @@ package alta.imobiliaria.domain;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
 
 @Data
 @Entity
 @Table(name = "agencies")
+@FilterDef(name = "agencyFilter", parameters = @ParamDef(name = "agencyId", type = Long.class))
 public class Agency {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
